a23a42410cd0c861ae97570acbe8e4b56057e4cc
sr2949@nau.edu <sr2949@nau.edu>
2023-03-26 14:02:52 -0700

    Added the ability to change the projectile from a ball (you can use a simple UI element to perform the switch; similar to how the camera is switched in the game) to a spear (use a simple cylindrical object for the spear) update the collisions and          physics when player switches the projectile.

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Slingshot : MonoBehaviour {
 	static private Slingshot S;
 	// поля, устанавливаемые в инспекторе Unity
 	[Header("Set in Inspector")]
 	public GameObject prefabProjectile;
+	public GameObject prefabSecondProjectile;
 	public float velocityMult = 8f;
+	public Text uitButton; // ссылка на дочерний объект Text в UIButton_View
 
 	//поля, устанавливаемые динамически
 	[Header("Set Dynamically")]
@@ -17,11 +21,48 @@ public class Slingshot : MonoBehaviour {
 	public bool aimingMode;
 
 	private Rigidbody projectileRigidbody;
+	private GameObject SetPrefab;
 
 	static public Vector3 LAUNCH_POS {
 		get {
 			if (S == null) return Vector3.zero;
 				return S.launchPos;
+		}
+	}
+	void Start()
+    {
+		prefabProjectile.SetActive(true);
+		prefabSecondProjectile.SetActive(false);
+		SetPrefab = prefabProjectile;
+		uitButton.text = "Switch To Sphere";
+	}
+
+	public void SwitchProjectile()
+    {
+		if (prefabProjectile.activeSelf)
+		{
+			// Switch to the spear
+			prefabProjectile.SetActive(false);
+			prefabSecondProjectile.SetActive(true);
+			SetPrefab = prefabSecondProjectile;
+			uitButton.text = "Switch To Ball";
+
+
+
+			// Enable spear physics
+			prefabSecondProjectile.GetComponent<Rigidbody>().isKinematic = false;
+		}
+		else
+		{
+			// Switch to the ball
+			prefabSecondProjectile.SetActive(false);
+			prefabProjectile.SetActive(true);
+			SetPrefab = prefabProjectile;
+			uitButton.text = "Switch To Sphere";
+
+
+			// Enable ball physics
+			prefabProjectile.GetComponent<Rigidbody>().isKinematic = false;
 		}
 	}
 
@@ -47,7 +88,7 @@ public class Slingshot : MonoBehaviour {
 	// игрок нажал кнопку мыши, когда указатель находился над рогаткой
 		aimingMode = true;
 		// создать снаряд
-		projectile = Instantiate(prefabProjectile) as GameObject;
+		projectile = Instantiate(SetPrefab) as GameObject;
 		// поместить в точку launchPoint
 		projectile.transform.position = launchPos;
 		// сделать его кинематическим
